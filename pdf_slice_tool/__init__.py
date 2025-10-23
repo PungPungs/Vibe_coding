41bd4a8aed37734adbb3036118eb119e2d5bee40
PungPung <han_thesis@naver.com>
2025-10-23 19:41:29 +0900

    Delete pdf_slice_tool directory

--- pdf_slice_tool/__init__.py
+++
@@ -1,6 +0,0 @@
-"""PDF Slice Tool - Split PDF files into individual pages for LangChain."""
-
-from .pdf_slicer import PDFSlicerTool, slice_pdf
-
-__version__ = "0.1.0"
-__all__ = ["PDFSlicerTool", "slice_pdf"]
